edd5903420262642491b5aa6bafbf5c59b59eb49
nina231e <112410101+nina231e@users.noreply.github.com>
2022-08-29 22:03:43 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "rumah.py",
+    version = "0.1",
+    py_modules = ['rumah'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "nina231e",
+    install_requires = ["pyngrok"]
+)
